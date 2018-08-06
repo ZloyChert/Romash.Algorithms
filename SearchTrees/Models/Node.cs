53fdc1641fd8fce0cea46683b90d255da945349f
Pavel <19860714r@gmail.com>
2018-08-06 13:13:25 +0300

    Auto stash before merge of "master" and "Romash.Algorithms/master"

---
+++ SearchTrees/Models/Node.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace SearchTrees.Models
+{
+    public class Node<TKey, TValue> : NodeBase<Node<TKey, TValue>, TKey, TValue>
+        where TKey : IComparable<TKey>
+    {
+    }
+}
